bee2fd18416bd09ac9eac3d30ba1a33c4a03a089
Dereck Lledo <derecklledo@gmail.com>
2018-03-27 17:23:42 -0400

    sansEspaces TEST - "une phrase avec 3 espaces, des mots et ensuite 3 espaces est valide "

--- src/app/shared/caracteres-validator.spec.ts
+++ src/app/shared/caracteres-validator.spec.ts
@@ -24,7 +24,14 @@ describe('sansEspaces Validator', () => {
         let validator = sansEspaceValidator.plage();
         let result = validator(control as AbstractControl);
         expect(result['plage']).toBe(true);
+    })
 
+    
+    it("une phrase avec 3 espaces, des mots et ensuite 3 espaces est valide ", () => {
+        let control = {value: "   Il y a 3 espaces au début et à la fin de la phrase.   "};
+        let validator = sansEspaceValidator.plage();
+        let result = validator(control as AbstractControl);
+        expect(result['plage']).toBe(true);
     })
 
 
